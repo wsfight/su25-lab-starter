fea819c5dba0548776da1336ee9e796819b34a13
larrywang <2813262902@qq.com>
2025-07-25 18:51:38 +0800

    "first commit copy.py"

--- lab00/code.py
+++ lab00/code.py
@@ -7,12 +7,15 @@ def get_airspeed_velocity_of(unladen_swallow):
 # pretend there's code here...
 
 def fizzbuzz(num):
-  if num == 3: # edit this line
+  if num % 3 == 0: # edit this line
     print(f"{num}: fizz")
-  if num == 5: # edit this line
+  if num % 5 == 0: # edit this line
     print(f"{num}: buzz")
-
+  if num % 3 == 0 and num % 5 == 0:
+    print(f"{num}: fizzbuzz")
+  if num % 3 != 0 and num % 5 != 0:
+    print()
 for i in range(1, 20):
-  fizzbuzz(i)
+    fizzbuzz(i)
 
 # pretend there's code here...
